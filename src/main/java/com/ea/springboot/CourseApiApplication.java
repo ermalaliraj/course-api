53d8a48d70ae2f4fcb0a4079232c67a3a5486545
Ermal <ermal.aliraj@gmail.com>
2017-09-05 23:00:57 +0200

    unused code

--- src/main/java/com/ea/springboot/CourseApiApplication.java
+++ src/main/java/com/ea/springboot/CourseApiApplication.java
@@ -4,8 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-import com.ea.springboot.topic.TopicController;
-
 @SpringBootApplication
 @ComponentScan(basePackages="com.ea") 
 public class CourseApiApplication {
